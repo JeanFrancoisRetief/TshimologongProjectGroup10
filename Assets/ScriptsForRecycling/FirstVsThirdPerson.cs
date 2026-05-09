3aed50a163e341ac5cd14930935cab264650a11b
JeanFrancoisRetief <2458318@students.wits.ac.za>
2026-05-09 15:00:56 +0200

    3rd person mode added

---
+++ Assets/ScriptsForRecycling/FirstVsThirdPerson.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class FirstVsThirdPerson : MonoBehaviour
+{
+    public bool isThirdPerson;
+
+    public GameObject ThirdPersonCharacter;
+    public GameObject ThirdPersonCam;
+
+    public GameObject FirstPersonCharacter;
+    public GameObject FirstPersonCam;
+    public GameObject FirstPersonCamHolder;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        isThirdPerson = false;
+
+        // Makes the cursor invisible
+        Cursor.visible = false;
+
+        // Optional: Locks the cursor to the center of the screen
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isThirdPerson)
+        {
+            ThirdPersonCharacter.SetActive(true);
+            ThirdPersonCam.SetActive(true);
+
+            FirstPersonCharacter.SetActive(false);
+            FirstPersonCam.SetActive(false);
+            FirstPersonCamHolder.SetActive(false);
+        }
+        else
+        {
+            ThirdPersonCharacter.SetActive(false);
+            ThirdPersonCam.SetActive(false);
+
+            FirstPersonCharacter.SetActive(true);
+            FirstPersonCam.SetActive(true);
+            FirstPersonCamHolder.SetActive(true);
+        }
+
+        
+
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if(isThirdPerson)
+            {
+                FirstPersonCharacter.transform.position = ThirdPersonCharacter.transform.position;
+                FirstPersonCharacter.transform.rotation = ThirdPersonCharacter.transform.rotation;
+            }
+            else
+            {
+                ThirdPersonCharacter.transform.position = FirstPersonCharacter.transform.position;
+                ThirdPersonCharacter.transform.rotation = FirstPersonCharacter.transform.rotation;
+            }
+
+
+                isThirdPerson = !isThirdPerson;
+        }
+    }
+}
