faf740f04a66b1cedc806f4a86d14cb94bd49350
JeanFrancoisRetief <2458318@students.wits.ac.za>
2026-06-04 11:45:03 +0200

    added all artifacts

--- Assets/ScriptsForArtifacts/ArtifactTrigger.cs
+++ Assets/ScriptsForArtifacts/ArtifactTrigger.cs
@@ -13,6 +13,16 @@ public class ArtifactTrigger : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        artifactsScript.Artifact001.SetActiveRecursively(false);
+        artifactsScript.Artifact002.SetActiveRecursively(false);
+        artifactsScript.Artifact003.SetActiveRecursively(false);
+        artifactsScript.Artifact004.SetActiveRecursively(false);
+        artifactsScript.Artifact005.SetActiveRecursively(false);
+        artifactsScript.Artifact006.SetActiveRecursively(false);
+        artifactsScript.Artifact007.SetActiveRecursively(false);
+        artifactsScript.Artifact008.SetActiveRecursively(false);
+        artifactsScript.Artifact009.SetActiveRecursively(false);
+        artifactsScript.Artifact010.SetActiveRecursively(false);
         
     }
 
@@ -35,12 +45,41 @@ public class ArtifactTrigger : MonoBehaviour
             {
                 artifactsScript.Artifact002.SetActiveRecursively(true);
             }
+            else if (ArtifactIndex == 3)
+            {
+                artifactsScript.Artifact003.SetActiveRecursively(true);
+            }
+            else if (ArtifactIndex == 4)
+            {
+                artifactsScript.Artifact004.SetActiveRecursively(true);
+            }
+            else if (ArtifactIndex == 5)
+            {
+                artifactsScript.Artifact005.SetActiveRecursively(true);
+            }
+            else if (ArtifactIndex == 6)
+            {
+                artifactsScript.Artifact006.SetActiveRecursively(true);
+            }
+            else if (ArtifactIndex == 7)
+            {
+                artifactsScript.Artifact007.SetActiveRecursively(true);
+            }
+            else if (ArtifactIndex == 8)
+            {
+                artifactsScript.Artifact008.SetActiveRecursively(true);
+            }
+            else if (ArtifactIndex == 9)
+            {
+                artifactsScript.Artifact009.SetActiveRecursively(true);
+            }
+            else if (ArtifactIndex == 10)
+            {
+                artifactsScript.Artifact010.SetActiveRecursively(true);
+            }
 
-            
 
-
-
-                currentObj.SetActiveRecursively(false);
+            currentObj.SetActiveRecursively(false);
         }
     }
 }
